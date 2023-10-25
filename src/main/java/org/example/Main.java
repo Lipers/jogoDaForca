b24c4b37932c9ca718cd02e485e997ea0720cef6
lipe <lipers20@hotmail.com>
2023-10-25 10:52:50 -0300

    para o portugues

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
-    static String hint = "";
+    static String dica = "";
     public static void main(String[] args) {
 
         System.out.println("Digite qual palavra será a palavra da forca.");
@@ -11,47 +11,47 @@ public class Main {
         var palavraForca = sc.nextLine().toLowerCase();
         int tentativas = 10;
 
-        String tentativasMessage = "Você tem %s tentativas para acertar a palavra da forca.\n";
-        System.out.printf(tentativasMessage, tentativas);
-        var hintBuffer = new StringBuffer();
+        String mensagemTentativas = "Você tem %s tentativas para acertar a palavra da forca.\n";
+        System.out.printf(mensagemTentativas, tentativas);
+        var dicaBuffer = new StringBuffer();
         for (char c: palavraForca.toCharArray()) {
-            hintBuffer.append("_");
+            dicaBuffer.append("_");
         }
-        hint = hintBuffer.toString().trim();
+        dica = dicaBuffer.toString().trim();
         while (tentativas > 0) {
             var input = sc.nextLine().toLowerCase();
             if (input.equals(palavraForca)) {
                 System.out.println("Parabéns, você ganhouuuuuuu!!!!!!!!!!!!!!!!");
                 return;
             } else if (input.length() == 1 && palavraForca.contains(input)) {
-                hint = revelarChar(input.charAt(0), palavraForca);
+                dica = revelarChar(input.charAt(0), palavraForca);
             } else {
                 --tentativas;
             }
 
-            System.out.println(hintFormatted());
-            System.out.printf(tentativasMessage, tentativas);
+            System.out.println(dicaFormatada());
+            System.out.printf(mensagemTentativas, tentativas);
         }
         System.out.println("Você perdeuuuuu, a palavra era: " + palavraForca);
     }
 
-    private static StringBuffer hintFormatted() {
-        var newHint = new StringBuffer();
-        for (char c: hint.toCharArray()) {
-            newHint.append(c).append(" ");
+    private static StringBuffer dicaFormatada() {
+        var novaDica = new StringBuffer();
+        for (char c: dica.toCharArray()) {
+            novaDica.append(c).append(" ");
         }
-        return newHint;
+        return novaDica;
     }
 
     private static String revelarChar(char input, String palavraForca) {
-        var newHint = new StringBuffer();
+        var novaDica = new StringBuffer();
         for (int i = 0; i < palavraForca.length(); i++) {
             if (input == palavraForca.charAt(i)) {
-                newHint.append(input);
+                novaDica.append(input);
             } else  {
-                newHint.append(hint.charAt(i));
+                novaDica.append(dica.charAt(i));
             }
         }
-        return newHint.toString();
+        return novaDica.toString();
     }
 }
